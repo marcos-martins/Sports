e092da69a13e0ee33855862f00137bc468166929
marcos-martins <m.martins@outlook.com>
2018-09-02 11:27:51 +0100

    Add project files.

---
+++ SportsStore.Domain/Abstract/IProductsRepository.cs
@@ -0,0 +1,19 @@
+﻿using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Domain.Abstract
+{
+    public interface IProductsRepository
+    {
+        IEnumerable<Product> Products { get; }
+
+        void SaveProduct(Product product);
+
+        Product DeleteProduct(int productID);
+    }
+}
+
